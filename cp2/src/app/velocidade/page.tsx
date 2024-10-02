1afc87099a5d98ac52506b73f8b95755f4df8c5e
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-10-02 11:36:02 -0300

    paginas alteradas e rotas adicionadas

---
+++ cp2/src/app/velocidade/page.tsx
@@ -0,0 +1,10 @@
+export default function Velocidade(){
+    return(
+        <div>
+            <h1>Velocidade e Trajetória de Vênus</h1>
+            <p>Velikovsky propôs que Vênus teria sido expulsa de Júpiter como um cometa e, em seguida, passou perto da Terra antes de<br/>
+              se estabelecer na sua órbita atual. Isso teria ocorrido num curto período de tempo, algo que desafiava as equações <br/>
+              de mecânica orbital clássica, que sugerem que as mudanças de órbita de planetas e corpos celestes são lentas e estáveis.</p>
+        </div>
+    )
+}
