ad29433d2c28049538f4de57f0bca86eed32d879
felipeprometti <felipe.prometti@gmail.com>
2024-10-02 08:45:37 -0300

    primeiro commit

---
+++ cp2/src/app/pages/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body></body>
+    </html>
+  )
+}
